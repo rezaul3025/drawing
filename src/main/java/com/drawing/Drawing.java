9673826082be77e51d5c8db088ed7050c8594d2f
rkarim <rezaul.karim@gmail.com>
2016-08-19 23:12:54 +0200

    init

--- src/main/java/com/drawing/Drawing.java
+++ src/main/java/com/drawing/Drawing.java
@@ -13,8 +13,8 @@ public class Drawing {
 
     public static void main(String[] arg) {
         StringBuffer sb = new StringBuffer();
-        int x1=2,y1=3,x2=14,y2=3;
-        int h = 10, w = 20;
+        int x1=10,y1=3,x2=10,y2=10;
+        int h = 20, w = 80;
         for (int i = 0; i <=h; i++) {
             for (int j = 0; j <=w; j++) {
                 if (i == 0) {
@@ -48,21 +48,48 @@ public class Drawing {
         //sb.deleteCharAt(24);
         //sb.insert(24, "Y");
         //sb.append("\n*").append("\n*").append("\n*").append("*****").append("\n*").append("\n*").append("\n*");
-        String lineCanvus= sb.toString();
+        String canvus= sb.toString();
         if(y1 == y2){
-            for(int l=x1;l<x2;l++){
+            for(int l=x1;l<=x2;l++){
                 String xy = "("+l+","+y1+")";
                 //sb.deleteCharAt(sb.indexOf(xy));
                 //sb.insert(sb.indexOf(xy), "X");
-                lineCanvus = lineCanvus.replace(xy, "X");
+                canvus = canvus.replace(xy, "X");
             }
+            String lineHCanvus = canvus;
             for(int lh = 0;lh<=h;lh++){
                 for(int lw=0;lw<=w;lw++){
                     String lhlw = "("+lw+","+lh+")";
-                    lineCanvus = lineCanvus.replace(lhlw, " ");
+                    lineHCanvus = lineHCanvus.replace(lhlw, " ");
                 }
             }
+            
+            System.out.println(lineHCanvus);
         }
-        System.out.println(lineCanvus);
+        
+        
+        
+        
+        if(x1 == x2){
+            for(int l=y1;l<=y2;l++){
+                String xy = "("+y1+","+l+")";
+                //sb.deleteCharAt(sb.indexOf(xy));
+                //sb.insert(sb.indexOf(xy), "X");
+                canvus = canvus.replace(xy, "X");
+            }
+            
+            String lineVCanvus= canvus;
+            
+            for(int lh = 0;lh<=h;lh++){
+                for(int lw=0;lw<=w;lw++){
+                    String lhlw = "("+lw+","+lh+")";
+                    lineVCanvus = lineVCanvus.replace(lhlw, " ");
+                }
+            }
+            
+            System.out.println(lineVCanvus);
+        }
+        
+        
     }
 }
